a642dc0dd645760df7646c3d563a909f1639a5d8
Márcio-Brust <brustmw21@gmail.com>
2023-11-17 16:41:44 -0300

    projetoTibau

---
+++ src/components/AccordionTable.tsx
@@ -0,0 +1,29 @@
+
+
+export const AccordionTable = () => {
+  return (
+    <>
+      <div className="collapse collapse-arrow bg-base-200">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">Tabela Grande</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-200">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">Tablela menor</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-200">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">Tabela menor</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/components/TabTables.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useRef } from "react";
+import { AccordionTable } from "./AccordionTable";
+
+export const TabTables = () => {
+  const divTabs = useRef<HTMLDivElement>(null);
+
+  const handleActive = ({ currentTarget }: React.MouseEvent) => {
+    const tabs = divTabs.current?.querySelectorAll("a");
+    if (!currentTarget.classList.contains("tab-active"))
+      tabs?.forEach((tab) => tab.classList.toggle("tab-active"));
+  };
+
+  return (
+    <section>
+      <div ref={divTabs} className="tabs tabs-boxed">
+        <a className="tab tab-active rounded-none" onClick={handleActive}>
+          ALL MATCHES
+        </a>
+        <a className="tab" onClick={handleActive}>
+          UPCOMING MATCHES
+        </a>
+      </div>
+      <AccordionTable />
+    </section>
+  );
+};
